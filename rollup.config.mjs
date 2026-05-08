68ffe3619235b48d9c85eac4a5c256c0ba879882
One <43485962+c-git@users.noreply.github.com>
2026-05-08 01:38:16 -0400

    Remove extra files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -13,29 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import cleanup from 'rollup-plugin-cleanup';
-import license from 'rollup-plugin-license';
-import prettier from 'rollup-plugin-prettier';
-import typescript from 'rollup-plugin-typescript2';
-import { fileURLToPath } from 'url';
+import cleanup from "rollup-plugin-cleanup";
+import typescript from "rollup-plugin-typescript2";
+import { fileURLToPath } from "url";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    dir: 'dist',
-    format: 'esm',
+    dir: "dist",
+    format: "esm",
   },
   plugins: [
-    cleanup({ comments: 'none', extensions: ['.ts'] }),
-    license({
-      banner: {
-        content: {
-          file: fileURLToPath(new URL('license-header.txt', import.meta.url)),
-        },
-      },
-    }),
+    cleanup({ comments: "none", extensions: [".ts"] }),
     typescript(),
-    prettier({ parser: 'typescript' }),
   ],
-  context: 'this',
+  context: "this",
 };
